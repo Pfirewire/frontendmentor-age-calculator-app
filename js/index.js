6a11c582fe358aa3432d46443111a20ce1da5af1
Steve Pflug <stephen@pflug.us>
2023-11-17 10:17:37 -0600

    completed validation logic and rendering

--- js/index.js
+++ js/index.js
@@ -9,6 +9,12 @@
     const daySpan = document.querySelector('#day-value');
     const monthSpan = document.querySelector('#month-value');
     const yearSpan = document.querySelector('#year-value');
+    const dayLabel = document.querySelector('#day-label');
+    const monthLabel = document.querySelector('#month-label');
+    const yearLabel = document.querySelector('#year-label');
+    const dayError = document.querySelector('#invalid-day');
+    const monthError = document.querySelector('#invalid-month');
+    const yearError = document.querySelector('#invalid-year');
     const submitBtn = document.querySelector('.arrow-wrapper');
 
     // Functions
@@ -51,26 +57,167 @@
         }
         return diff;
     }
-
     const clearInputs = () => {
         dayInput.value = "";
         monthInput.value = "";
         yearInput.value = "";
     }
-
     const setDateValues = amountOfTime => {
         daySpan.innerText = amountOfTime.days;
         monthSpan.innerText = amountOfTime.months;
         yearSpan.innerText = amountOfTime.years;
     };
-
+    const hasBlank = (year, month, day) => {
+        return year === '' || month === '' || day === '';
+    };
+    const hasSpecificError = (currentYear, year, month, day) => {
+        const daysInMonth = [
+            31,
+            (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 29 : 28,
+            31,
+            30,
+            31,
+            30,
+            31,
+            31,
+            30,
+            31,
+            30,
+            31
+        ];
+        return currentYear < year
+            || month < 0
+            || month > 12
+            || day < 1
+            || day > 31
+            || day > daysInMonth[month - 1];
+    };
+    const isValidDate = (currentDate, year, month, day) => {
+        const currentYear = currentDate.getFullYear();
+        let isValid = true;
+        if(
+            hasBlank(year, month, day)
+            || hasSpecificError(currentYear, year, month, day)
+            || currentDate.getTime() < (new Date(year, month - 1, day)).getTime()
+        ) {
+            isValid = false;
+        }
+        return isValid;
+    };
+    const renderNoYearErrors = () => {
+        yearLabel.style.color = 'var(--smokey-grey)';
+        yearInput.style.border = '1px solid var(--light-grey)';
+        yearError.innerText = '';
+        yearError.style.display = 'none';
+    };
+    const renderNoMonthErrors = () => {
+        monthLabel.style.color = 'var(--smokey-grey)';
+        monthInput.style.border = '1px solid var(--light-grey)';
+        monthError.innerText = '';
+        monthError.style.display = 'none';
+    };
+    const renderNoDayErrors = () => {
+        dayLabel.style.color = 'var(--smokey-grey)';
+        dayInput.style.border = '1px solid var(--light-grey)';
+        dayError.innerText = '';
+        dayError.style.display = 'none';
+    };
+    const renderNoErrors = () => {
+        renderNoYearErrors();
+        renderNoMonthErrors();
+        renderNoDayErrors();
+    };
+    const renderYearError = message => {
+        yearLabel.style.color = 'var(--light-red)';
+        yearInput.style.border = '1px solid var(--light-red)';
+        yearError.innerText = message;
+        yearError.style.display = 'block';
+    };
+    const renderMonthError = message => {
+        monthLabel.style.color = 'var(--light-red)';
+        monthInput.style.border = '1px solid var(--light-red)';
+        monthError.innerText = message;
+        monthError.style.display = 'block';
+    };
+    const renderDayError = message => {
+        dayLabel.style.color = 'var(--light-red)';
+        dayInput.style.border = '1px solid var(--light-red)';
+        dayError.innerText = message;
+        dayError.style.display = 'block';
+    };
+    const renderInvalidDate = message => {
+        renderYearError('');
+        renderMonthError('');
+        renderDayError(message);
+    }
+    const renderInvalidDateMessages = (currentDate, year, month, day) => {
+        const currentYear = currentDate.getFullYear();
+        const daysInMonth = [
+            31,
+            (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 29 : 28,
+            31,
+            30,
+            31,
+            30,
+            31,
+            31,
+            30,
+            31,
+            30,
+            31
+        ];
+        if(hasBlank(year, month, day)) {
+            if(year === '') {
+                renderYearError('This field is required');
+            } else {
+                renderNoYearErrors();
+            }
+            if(month === '') {
+                renderMonthError('This field is required');
+            } else {
+                renderNoMonthErrors();
+            }
+            if(day === '') {
+                renderDayError('This field is required');
+            } else {
+                renderNoDayErrors();
+            }
+        }
+        else if(hasSpecificError(currentYear, year, month, day)) {
+            if(currentYear < year) {
+                renderYearError('Must be in the past');
+            } else {
+                renderNoYearErrors();
+            }
+            if(month < 0 || month > 12) {
+                renderMonthError('Must be a valid month');
+            } else {
+                renderNoMonthErrors();
+            }
+            if(day < 1 || day > 31 || day > daysInMonth[month - 1]) {
+                renderDayError('Must be a valid day');
+            } else {
+                renderNoDayErrors();
+            }
+        }
+        else if (currentDate.getTime() < (new Date(year, month - 1, day)).getTime()) {
+            renderInvalidDate('Must be in the past');
+        }
+        else {
+            renderInvalidDate('Must be a valid date');
+        }
+    };
     const handleSubmitClick = () => {
-        console.log("Submit button clicked");
-        const userDate = new Date(yearInput.value, monthInput.value - 1, dayInput.value);
-        const amountOfTime = dateDiff(userDate, new Date());
-        console.log(amountOfTime);
-        setDateValues(amountOfTime);
-        clearInputs();
+        const currentDate = new Date();
+        if(isValidDate(currentDate, yearInput.value, monthInput.value, dayInput.value)) {
+            const userDate = new Date(yearInput.value, monthInput.value - 1, dayInput.value);
+            const amountOfTime = dateDiff(userDate, currentDate);
+            setDateValues(amountOfTime);
+            renderNoErrors();
+            clearInputs();
+        } else {
+            renderInvalidDateMessages(currentDate, yearInput.value, monthInput.value, dayInput.value);
+        }
     };
 
     // Events
